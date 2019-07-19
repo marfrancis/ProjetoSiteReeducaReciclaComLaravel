c4a599ddc7aecefa1a1f27aa5042308f0a6b5eb7
marfrancis <marcelo@mediasolutionproducoes.com.br>
2019-07-19 14:34:33 -0300

    criação de rotas, links imagens, css e java script

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+Route::get('/', 'HomeController@home');
